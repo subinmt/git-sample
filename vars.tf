a5b3d13fed634d40c8a6d3a10cd845f819bfb833
subinmt <37261883+subinmt@users.noreply.github.com>
2020-06-16 17:04:39 +0530

    test

--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@ variable "env_vars" {
 }
 
 variable "env_vpc" {
-  type = "map"
+  type = map
   default = {
     dev     = "dev"
     pprod   = "pre-prod"
@@ -11,7 +11,7 @@ variable "env_vpc" {
 }
 
 variable "security_group_ids" {
-  type = "map"
+  type = map
   default = {
     dev     = ["sg-0ea00f5503f8095d6"]
     pprod   = ["sg-0ea00f5503f8095d6"]
@@ -20,7 +20,7 @@ variable "security_group_ids" {
 }
 
 variable "sg_wrk_nodes_ids" {
-  type = "map"
+  type = map
   default = {
     dev     = ["sg-08abb466319b6f316", "sg-054fa00351b36ce25"]
     pprod   = ["sg-08abb466319b6f316", "sg-054fa00351b36ce25"]
@@ -29,31 +29,28 @@ variable "sg_wrk_nodes_ids" {
 }
 
 variable "iam_cluster_role" {
-  type = "map"
+  type = map
   default = {
     dev     = "arn:aws:iam::861112368680:role/MyEKS-Role"
     pprod   = "arn:aws:iam::861112368680:role/MyEKS-Role"
     prod    = "arn:aws:iam::861112368680:role/MyEKS-Role"
-    type    = string
   }
 }
 
-variable "aws_iam_instance_profile" {
-  type = "map"
-  default = {
-    dev     = "arn:aws:iam::861112368680:instance-profile/Ec2Admin-Bastion"
-    pprod   = "arn:aws:iam::861112368680:instance-profile/Ec2Admin-Bastion"
-    prod    = "arn:aws:iam::861112368680:instance-profile/Ec2Admin-Bastion"
-    type = string
-  }
-}
+#variable "aws_iam_instance_profile" {
+#  type = map
+#  default = {
+#    dev     = "arn:aws:iam::861112368680:instance-profile/Ec2Admin-Bastion"
+#    pprod   = "arn:aws:iam::861112368680:instance-profile/Ec2Admin-Bastion"
+#    prod    = "arn:aws:iam::861112368680:instance-profile/Ec2Admin-Bastion"
+#  }
+#}
 
-variable "iam_worker_role" {
-  type = "map"
-  default = {
-    dev     = "arn:aws:iam::861112368680:role/worker-nodes-role"
-    pprod   = "arn:aws:iam::861112368680:role/worker-nodes-role"
-    prod    = "arn:aws:iam::861112368680:role/worker-nodes-role"
-    type = string
-  }
-}
+#variable "iam_worker_role" {
+#  type = map
+#  default = {
+#    dev     = "arn:aws:iam::861112368680:role/worker-nodes-role"
+#    pprod   = "arn:aws:iam::861112368680:role/worker-nodes-role"
+#    prod    = "arn:aws:iam::861112368680:role/worker-nodes-role"
+#  }
+#}
