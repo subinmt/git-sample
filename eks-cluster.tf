7bc4c8681ea88c9f1a39253e81749092f6e020b2
subinmt <37261883+subinmt@users.noreply.github.com>
2020-06-21 22:26:40 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -2,8 +2,8 @@ data "aws_vpc" "eks_vpc" {
   filter {
     name = "tag:Environment"
     #values = var.env_vpc[var.env]
-    #values = lookup(var.env_vpc, var.env)
-    values = var.env_vpc["${var.env}"]
+    values = lookup(var.env_vpc, var.env, null)
+    #values = var.env_vpc["${var.env}"]
   }
 }
 
