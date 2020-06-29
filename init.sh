8c3388cbca8edd783bf39df1427319d92d8deffd
subinmt <37261883+subinmt@users.noreply.github.com>
2020-06-29 17:07:45 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,9 +9,9 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/l
 
 chmod +x ./kubectl
 
-sudo mv ./kubectl /usr/local/bin/kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 
-kubectl version --client
+kubectl version --short --client
 
 
 
