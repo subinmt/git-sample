46cc15296b1b096230a47f97649c53c592d56165
subinmt <37261883+subinmt@users.noreply.github.com>
2020-06-29 17:17:34 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,11 +5,11 @@ sudo adduser jenkins$d
 sudo cd /home/jenkins$d
 
 #To get kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/linux/amd64/kubectl
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v"$1".0/bin/linux/amd64/kubectl
 
-chmod +x ./kubectl
+sudo chmod +x ./kubectl
 
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+sudo mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 
 kubectl version --short --client
 
@@ -17,19 +17,19 @@ kubectl version --short --client
 
 wget https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.3.0/heptio-authenticator-aws_0.3.0_linux_amd64 -O heptio-authenticator-aws
 
-chmod +x heptio-authenticator-aws
+sudo chmod +x heptio-authenticator-aws
 
-sudo mv heptio-authenticator-aws /usr/local/bin/
+sudo mkdir -p $HOME/bin && cp ./heptio-authenticator-aws $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 
 heptio-authenticator-aws help
 
 
-if [ $1 == "dev" ]
+if [ $2 == "dev" ]
 then
     echo "Dev"
     sed -i 's|REPLACEME|arn:aws:iam::861112368680:role/worker-nodes-role|g' config-map-aws-auth.yaml
  
-elif [ $1 == "pprod" ]
+elif [ $2 == "pprod" ]
 then
     echo "Preprod"
     sed -i 's|REPLACEME|arn:aws:iam::861112368680:role/worker-nodes-role2|g' config-map-aws-auth.yaml
