912cbcd422221bff6319f89a8fcd6efaec78eefe
subinmt <37261883+subinmt@users.noreply.github.com>
2020-06-29 15:11:06 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -32,27 +32,3 @@ KUBECONFIG
 output "kubeconfig" {
   value = local.kubeconfig
 }
-
-# Join configuration
-
-locals {
-  config-map-aws-auth = <<CONFIGMAPAWSAUTH
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: aws-auth
-  namespace: kube-system
-data:
-  mapRoles: |
-    - rolearn: lookup(var.iam_worker_role, var.env, null)
-      username: system:node:{{EC2PrivateDNSName}}
-      groups:
-        - system:bootstrappers
-        - system:nodes
-CONFIGMAPAWSAUTH
-
-}
-
-output "config-map-aws-auth" {
-value = local.config-map-aws-auth
-}
