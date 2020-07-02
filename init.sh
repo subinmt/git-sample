22944f0055ff6d5384ad2bf1084f8d7c7efde2f8
subinmt <37261883+subinmt@users.noreply.github.com>
2020-07-02 12:54:34 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -56,4 +56,7 @@ sudo mv config-map-aws-auth.yaml /home/jenkins$d/config-map-aws-auth.yaml
 #sudo -H -u jenkins$d bash -c '/home/jenkins$d/bin/kubectl create -f /home/jenkins$d/config-map-aws-auth.yaml'
 
 sudo su -l -p jenkins$d
+
+echo 'export PATH=$PATH:/home/jenkins$d/bin' >> /home/jenkins$d/.bashrc
+source ~/.bashrc
 /home/jenkins$d/bin/kubectl create -f /home/jenkins$d/config-map-aws-auth.yaml
